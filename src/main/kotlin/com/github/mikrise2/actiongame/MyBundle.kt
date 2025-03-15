0c4db9de2ac6b15e9d4dddfa4486058eee4d0031
GitHub Action <action@github.com>
2025-03-15 19:05:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/mikrise2/actiongame/MyBundle.kt
+++ src/main/kotlin/com/github/mikrise2/actiongame/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mikrise2.actiongame
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mikrise2/actiongame/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mikrise2/actiongame/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mikrise2.actiongame.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mikrise2.actiongame.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
